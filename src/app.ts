f1dea01907f4d5d47cc1ca21851309dd1abf7cfb
oishi-riku <soukei.creator@gmail.com>
2022-09-10 00:39:29 +0900

    Update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import TestRoute from './routes/test/index';
+
 if (process.env.NODE_ENV !== 'production') dotenv.config();
 
 const app = express();
@@ -9,6 +11,8 @@ const PORT = process.env.PORT;
 app.get('/', (req, res) => {
     res.end('hello world');
 });
+
+app.use('/test/', TestRoute);
 
 app.listen(PORT, () => {
     console.log(`start ${PORT}`);
